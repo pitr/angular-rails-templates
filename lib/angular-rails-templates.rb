6b17ec36b5961c4c3d8b054acc4ab8757452ae12
Jeremy Ebler <jebler@gmail.com>
2014-04-23 16:45:49 -0700

    minor fix: AngularRailsTemplates::VERSION does not autoload

--- lib/angular-rails-templates.rb
+++ lib/angular-rails-templates.rb
@@ -6,7 +6,7 @@ module AngularRailsTemplates
   autoload :BaseTemplate    , 'angular-rails-templates/base_template'
   autoload :SlimTemplate    , 'angular-rails-templates/slim_template'
   autoload :HamlTemplate    , 'angular-rails-templates/haml_template'
-  autoload :Version         , 'angular-rails-templates/version'
+  autoload :VERSION         , 'angular-rails-templates/version'
 end
 
 require 'angular-rails-templates/engine'
