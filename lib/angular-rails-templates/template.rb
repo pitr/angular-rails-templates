a873ee33836d5cbb02e73dc2b6731ec5aab4c981
Rodik <i6@bk.ru>
2015-03-28 15:30:44 +0200

    Abitily to configure inside paths where templates could be located

--- lib/angular-rails-templates/template.rb
+++ lib/angular-rails-templates/template.rb
@@ -11,8 +11,8 @@ module AngularRailsTemplates
     end
 
     def prepare
-      # we only want to process html assets inside Rails.root/app/assets
-      @asset_inside_rails_root = file.match "#{Rails.root.join 'app', 'assets'}"
+      # we only want to process html assets inside Rails.root/app/assets and other folders specified in configuration
+      @asset_inside_rails_root = configuration.inside_paths.any? { |folder| file.match folder.to_s }
 
       if configuration.htmlcompressor and @asset_inside_rails_root
         @data = compress data
