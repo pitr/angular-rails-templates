bee52a02bc84fd4d5235d58b8da29715c9efc040
Jason Clark <jason@jjcconsultingllc.com>
2016-01-08 09:55:35 -0800

    Only include HAML and Slim component when using HAML or Slim

--- lib/angular-rails4-templates/slim_processor.rb
+++ lib/angular-rails4-templates/slim_processor.rb
@@ -1,15 +1,17 @@
-require 'angular-rails4-templates/compact_javascript_escape'
-require 'slim'
+if defined? Slim
+  require 'angular-rails4-templates/compact_javascript_escape'
+  require 'slim'
 
-module AngularRails4Templates
-  class SlimProcessor < Processor
+  module AngularRails4Templates
+    class SlimProcessor < Processor
 
-    include CompactJavaScriptEscape
+      include CompactJavaScriptEscape
 
-    def render_html(input)
-      template = input[:data]
-      output = Slim::Template.new { template }.render
-      escape_javascript output
+      def render_html(input)
+        template = input[:data]
+        output = Slim::Template.new { template }.render
+        escape_javascript output
+      end
     end
   end
 end
